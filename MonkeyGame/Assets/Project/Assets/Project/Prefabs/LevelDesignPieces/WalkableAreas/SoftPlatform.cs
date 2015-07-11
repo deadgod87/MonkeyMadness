1de656a618051a0422a9a9e99fca56733f7f7c3b
deadgod87 <brandon.shaw87@fullsail.edu>
2015-07-11 15:31:07 -0500

    fixed platforms
    Player can now jump through the bottom of platforms to land on top of
    them.

---
+++ MonkeyGame/Assets/Project/Assets/Project/Prefabs/LevelDesignPieces/WalkableAreas/SoftPlatform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoftPlatform : MonoBehaviour {
+
+    private Collider2D parentCol;
+
+    void Start()
+    {
+        parentCol = transform.parent.GetComponent<Collider2D>();//GetComponentInParent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Physics2D.IgnoreCollision(col.GetComponent<Collider2D>(), parentCol, true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Physics2D.IgnoreCollision(col.GetComponent<Collider2D>(), parentCol, false);
+        }
+    }
+}
--- MonkeyGame/Assets/Project/Assets/Project/Prefabs/Monkey/PlayerController.cs
+++ MonkeyGame/Assets/Project/Assets/Project/Prefabs/Monkey/PlayerController.cs
@@ -6,19 +6,21 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField] private float maxRunSpeed = 6.0f;
     [SerializeField] private float runAccel = 0.5f;
-    [SerializeField] private float jumpAccel = 0.3f;
+    //[SerializeField] private float jumpAccel = 0.3f; //Will use this if we decide to have an extended jump
     [SerializeField] private float initJumpSpeed = 3.0f;
     [SerializeField] private Transform groundCheck;
     [SerializeField] float groundCheckRad = 0.2f;
     [SerializeField] LayerMask walkableLayer;
 
-    private bool myDirection = true; //Determines players facing direction so the sprite matches. true = right(May not use this)
     private bool canJump = true;
     private bool onGround = false;
-    private float timeHoldingInput = 0.0f;
+    //private float timeHoldingInput = 0.0f; //Will use this if we decide to have an extended jump
 
     private Animator myAnim; // access animator component
     private Rigidbody2D rB; // used to access the rigidbody component
+
+
+    //----------------------------------------Start and Update--------------------------------------------------------------
 
 	// Use this for initialization
 	void Start ()
@@ -33,6 +35,7 @@ public class PlayerController : MonoBehaviour {
     {
         MovementControl();
 	}
+    //--------------------------------------------Movement Control----------------------------------------------------------
 
     //Handles all the movement for the monkey
     private void MovementControl()
@@ -58,20 +61,17 @@ public class PlayerController : MonoBehaviour {
             }
         }
 
-      
 
-		float direction = Input.GetAxis("Horizontal"); //The float value of the horizontal input
+		float direction = Input.GetAxis("Horizontal"); //The float value of the horizontal input; -1 = left, 0 = idle, 1 = right
 		
 		Vector2 accel = new Vector2(runAccel * direction, 0); //The acceleration of the players movement
 
 		if(direction > 0) //If true, player is moving right
 		{
-			myDirection = true;
             transform.localScale = new Vector3(-5, transform.localScale.y, transform.localScale.z);
 		}
 		else if(direction < 0) //if true, player is moving left
 		{
-			myDirection = false;
             transform.localScale = new Vector3(5, transform.localScale.y, transform.localScale.z);
 		}
 
@@ -98,11 +98,15 @@ public class PlayerController : MonoBehaviour {
         
 	}
 
+    //-----------------------------------------Jumping-------------------------------------------------------------
+
    private void HandleJumping()
     {
        // myAnim.SetBool("Jumping", true);
         rB.velocity = new Vector2(rB.velocity.x, initJumpSpeed); 
     }
+
+    //------------------------------------------Check Ground------------------------------------------------------------
 
     private void CheckForGround()
     {
