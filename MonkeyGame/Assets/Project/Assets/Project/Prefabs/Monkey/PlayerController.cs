7a178be6dabcb12367437bda5ec19172a3b11616
deadgod87 <brandon.shaw87@fullsail.edu>
2015-07-21 22:06:36 -0500

    moved stuff

--- MonkeyGame/Assets/Project/Assets/Project/Prefabs/Monkey/PlayerController.cs
+++ MonkeyGame/Assets/Project/Assets/Project/Prefabs/Monkey/PlayerController.cs
@@ -123,8 +123,6 @@ public class PlayerController : MonoBehaviour {
 
         //------Throw Stuff-----------//
 
-        mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-
         if(Input.GetButton("Fire1"))
         {
             if (throwTimer >= throwDelay)
